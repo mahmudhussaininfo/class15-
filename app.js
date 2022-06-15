deb207749b563abb8a93fdaf9c2818e166e3e9e6
Mahmud <77590197+mahmudhussaininfo@users.noreply.github.com>
2022-06-15 15:13:23 +0600

    area calculator

--- app.js
+++ app.js
@@ -1,103 +1,177 @@
 
+// Self Made
 
 // const age_form = document.getElementById('age_form');
 // const msg = document.querySelector('.msg');
 // const msgfooter = document.querySelector('.msgfooter');
 
-
 // age_form.onsubmit = (event) =>{
 
-//     // form re bondo kore raka
 //     event.preventDefault();
 
-//     // sob input re dora
-//     let name = age_form.querySelector('input[name ="name"]');
-//     let age = age_form.querySelector('input[name ="age"]');
+//     let name = age_form.querySelector('input[name = "name"]');
+//     let age = age_form.querySelector('input[name = "age"]');
 //     let gender = age_form.querySelectorAll('input[name ="gender"]');
-    
+
+
 //     let gender_name = '';
-
-//     // gender dora dori
-//     gender.forEach(type =>{
-
-//         if(type.checked){
-//             gender_name = type.value;
-            
+//     gender.forEach(item =>{
+        
+//         if(item.checked){
+//             gender_name = item.value; 
 //         }
 //     });
 
-    
+//     // console.log(gender_name);
 
-//     if (name.value == '' || age.value == '' || gender_name == ''){
 
-//         msg.innerHTML = setAlert('tiktak moto bowaite oibo');
+//     if(name.value == '' || age.value == '' || gender_name == ''){
         
-//     } else if( agecheck (age.value) == false ){
+//         msg.innerHTML = setAlert('Data is not valid');
 
-//         msg.innerHTML = setAlert('dur beta boyosh bowani hoise na', 'warning');
+//     }else if(namecheck(name.value) == false){
 
-//     } else if(namecheck(name.value) == false){
+//         msg.innerHTML = setAlert('Your Name is not correct');
 
-//         msg.innerHTML = setAlert('duro name o hikse na abo', 'warning');
+//     }else if (agecheck(age.value) == false){
 
+//         msg.innerHTML = setAlert('Your Age is not correct');
 
-//     } else {
-//         msg.innerHTML = setAlert('tik ase ostir hoise', 'success');
+//     } else{
 
-//         msgfooter.innerHTML = marriagecheck(name.value, age.value, gender_name);
-        
+//         msgfooter.innerHTML= marriagecheck(name.value,age.value,gender_name);
+
+//         msg.innerHTML = setAlert('Data Successfully valid', 'success');
 //     }
 
-    
+
+
+
 // }
 
 
-// Self Made
+// Corrency converter
 
-const age_form = document.getElementById('age_form');
-const msg = document.querySelector('.msg');
+
+// const currency_form = document.getElementById('currency_form');
+// const msg = document.querySelector('.msg');
+// const msgfooter = document.querySelector('.msgfooter');
+
+// currency_form.onsubmit = (c)=>{
+//     c.preventDefault();
+
+
+//     let amount = currency_form.querySelector('input[name ="amount"]');
+//     let currency = currency_form.querySelector('select[name = "currency"]');
+
+
+//     if( amount.value == '' || currency.value == ''){
+//         msg.innerHTML = setAlert('bowani hoise na tik tak');
+//     }else{
+//         msg.innerHTML = setAlert('ostir hoise sob tik tak!', 'success');
+
+//         let rate = 0;
+
+//         switch(currency.value){
+
+//             case 'USD' :
+//             rate = 89;
+//             break;
+
+//             case 'cad' :
+//             rate = 70;
+//             break;
+//             case 'euro' :
+//             rate = 89;
+//             break;
+//             case 'pound' :
+//             rate = 111;
+//             break;
+//             case 'rupee' :
+//             rate = 2;
+//             break;
+
+//         }
+
+//         msgfooter.innerHTML = setAlert(` ${amount.value} ${currency.value} = ${amount.value*rate} BDT`, 'success');
+
+
+//     }
+
+// }
+
+const area_type = document.getElementById('area_type');
+const rac = document.querySelector('.rac');
+const cir = document.querySelector('.cir');
+const tri = document.querySelector('.tri');
+const squ = document.querySelector('.squ');
 const msgfooter = document.querySelector('.msgfooter');
 
-age_form.onsubmit = (event) =>{
-
-    event.preventDefault();
-
-    let name = age_form.querySelector('input[name = "name"]');
-    let age = age_form.querySelector('input[name = "age"]');
-    let gender = age_form.querySelectorAll('input[name ="gender"]');
+area_type.onchange = (p) =>{
+    p.preventDefault();
 
 
-    let gender_name = '';
-    gender.forEach(item =>{
-        
-        if(item.checked){
-            gender_name = item.value; 
-        }
-    });
+    if( area_type.value == 'Circle'){
+        cir.style.display = 'block';
+    }else{
+        cir.style.display = '';
+    }
 
-    // console.log(gender_name);
+    if( area_type.value == 'Square'){
+        squ.style.display = 'block';
+    }else{
+        squ.style.display = '';
+    }
 
+    if( area_type.value == 'Reactangle'){
+        rac.style.display = 'block';
+    }else{
+        rac.style.display = '';
+    }
 
-    if(name.value == '' || age.value == '' || gender_name == ''){
-        
-        msg.innerHTML = setAlert('Data is not valid');
-
-    }else if(namecheck(name.value) == false){
-
-        msg.innerHTML = setAlert('Your Name is not correct');
-
-    }else if (agecheck(age.value) == false){
-
-        msg.innerHTML = setAlert('Your Age is not correct');
-
-    } else{
-
-        msgfooter.innerHTML= marriagecheck(name.value,age.value,gender_name);
-
-        msg.innerHTML = setAlert('Data Successfully valid', 'success');
+    if( area_type.value == 'Triangle'){
+        tri.style.display = 'block';
+    }else{
+        tri.style.display = '';
     }
 
 
+}
 
+//********** Form Submit ********* //
+
+const area_form = document.getElementById('area_form');
+
+area_form.onsubmit = (tt) =>{
+
+    tt.preventDefault();
+
+    if(area_type.value == 'Reactangle'){
+
+        let lenth = area_form.querySelector('input[name = "lenth"]').value;
+        let width = area_form.querySelector('input[name = "width"]').value;
+
+        msgfooter.innerHTML = areacal(area_type.value, lenth, width);
+
+    }else if ( area_type.value == 'Triangle'){
+
+        let base = area_form.querySelector('input[name = "base"]').value;
+        let height = area_form.querySelector('input[name = "height"]').value;
+
+        msgfooter.innerHTML = areacal(area_type.value, base, height);
+
+    }else if ( area_type.value == 'Square'){
+
+        let bahu = area_form.querySelector('input[name = "bahu"]').value;
+
+        msgfooter.innerHTML = areacal(area_type.value, bahu);
+
+    }else if ( area_type.value == 'Circle'){
+
+        let red = area_form.querySelector('input[name = "red"]').value;
+
+        msgfooter.innerHTML = areacal(area_type.value, red);
+
+    }
 
 }
--- function.js
+++ function.js
@@ -7,44 +7,75 @@ const setAlert = (msg, color = 'danger') =>{
     return `<p class ="alert alert-${color} d-flex justify-content-between">${msg} <button data-bs-dismiss ="alert" class = "btn-close"></button></p>`;
 }
 
-/**
- * Name Check
- */
+// /**
+//  * Name Check
+//  */
 
-const namecheck = (namee)=>{
-    let namepattern = /^[A-Za-z ]{3,20}$/;
-    return namepattern.test(namee);
-} 
+// const namecheck = (namee)=>{
+//     let namepattern = /^[A-Za-z ]{3,20}$/;
+//     return namepattern.test(namee);
+// } 
 
-/**
- * Age Check
- */
+// /**
+//  * Age Check
+//  */
 
-const agecheck = (age) =>{
-    let agepattern = /^[0-9]{1,3}$/;
-    return agepattern.test(age);
-}
+// const agecheck = (age) =>{
+//     let agepattern = /^[0-9]{1,3}$/;
+//     return agepattern.test(age);
+// }
 
 
-const marriagecheck = (name,age,gender) =>{
+// const marriagecheck = (name,age,gender) =>{
     
-    if(gender == 'male'){
+//     if(gender == 'male'){
 
 
-        let marriage_age = 21;
+//         let marriage_age = 21;
 
-        if( age >= marriage_age){
-            return setAlert (`Hello ${name} Bro, Congretulation Your marriage is coming soon`, 'success');
-        }else{
-            return setAlert (`Hello ${name} Babu, Sorry! You are not ready to marriage, please wait for ${marriage_age - age} Years`);
-        }
-    }else{
-        let marriage_age = 18;
+//         if( age >= marriage_age){
+//             return setAlert (`Hello ${name} Bro, Congretulation Your marriage is coming soon`, 'success');
+//         }else{
+//             return setAlert (`Hello ${name} Babu, Sorry! You are not ready to marriage, please wait for ${marriage_age - age} Years`);
+//         }
+//     }else{
+//         let marriage_age = 18;
 
-        if( age>= marriage_age){
-            return setAlert (`Hello ${name} Apu, Congretulation Your marriage is coming soon`, 'success');
-        }else{
-            return setAlert (`Hello ${name} shuna Babu, Sorry! You are not ready to marriage, please wait for ${marriage_age - age} Years`);
-        }
+//         if( age>= marriage_age){
+//             return setAlert (`Hello ${name} Apu, Congretulation Your marriage is coming soon`, 'success');
+//         }else{
+//             return setAlert (`Hello ${name} shuna Babu, Sorry! You are not ready to marriage, please wait for ${marriage_age - age} Years`);
+//         }
+//     }
+// }
+
+
+
+
+
+ 
+/**
+ * Area Calculator
+ */
+
+const areacal = (type, val1, val2) =>{
+
+    if(type == 'Reactangle'){
+
+        return setAlert (` The area of a Reactangle is ${ val1 * val2}`, 'success');
+
+    }else if(type == 'Triangle'){
+
+        return setAlert (` The area of a Triangle is ${ 0.5* val1 * val2}`, 'success');
+
+    }else if (type == 'Square'){
+
+        return setAlert (` The area of a Square is ${ val1 * val1 }`, 'success');
+        
+    }else if (type == 'Circle'){
+
+        return setAlert (` The area of a Circle is ${ 3.1416* val1 * val1 }`, 'success');
+
     }
+
 }
